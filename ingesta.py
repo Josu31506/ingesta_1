49febf997bfa6ce674e7d3172aa0db43493a0a3e
Josu31506 <josuehernandezyataco@gmail.com>
2025-09-27 03:37:16 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,10 +1,12 @@
 import boto3
 
 ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+nombreBucket = "farmacia-ds"
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+response = s3.upload_file(ficheroUpload, nombreBucket, 
+                          "medicinas/" + 
+                          ficheroUpload)
 print(response)
 
 print("Ingesta completada")
